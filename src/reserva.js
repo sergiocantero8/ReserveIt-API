1af05921e7c0d3c55b1eaf53d60c5490d7edff13
sergiocantero8 <sergiocantero8@gmail.com>
2020-10-13 18:40:52 +0200

    Clase actualizada con getter closes #5

--- src/reserva.js
+++ src/reserva.js
@@ -1,9 +1,44 @@
-function reserva () {
-    var tipo;
-    var duracion;
-    var fecha;
-    var precio;
-    var id;
-    var ubicacion;
-    var usuario;
+/* Clase que representa la reserva de un usuario */
+
+class Reserva {
+
+
+    constructor(tipo, duracion, fecha, precio, id, ubicacion, usuario){
+        this.tipo = tipo;
+        this.duracion = duracion;
+        this.fecha= fecha;
+        this.precio = precio;
+        this.id = id;
+        this.ubicacion = ubicacion;
+        this.usuario = usuario;
+
+    }
+
+    get tipo() {
+        return this.tipo;
+    }
+
+    get duracion() {
+        return this.duracion;
+    }
+
+    get fecha() {
+        return this.fecha;
+    }
+
+    get precio() {
+        return this.precio;
+    }
+
+    get id() {
+        return this.id;
+    }
+
+    get ubicacion() {
+        return this.ubicacion;
+    }
+
+    get usuario() {
+        return this.usuario;
+    }
 }
