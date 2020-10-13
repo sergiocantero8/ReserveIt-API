1f1954c3414684d7e4d1e59ab39383314a180d34
sergiocantero8 <sergiocantero8@gmail.com>
2020-10-13 18:48:24 +0200

    Clase actualizada con getter closes #4

--- src/usuario.js
+++ src/usuario.js
@@ -1,13 +1,50 @@
-function usuario () {
 
-    var nombre;
-    var apellidos;
-    var telefono;
-    var email;
-    var dni;
-    var fecha_nacimiento;
-    var lugar_nacimiento;
-	 var nacionalidad;
+/* Clase que representa los datos del usuario del sistema */
+
+class Usuario {
 
 
+    constructor(nombre, apellidos, telefono, email, dni, fecha_nacimiento, lugar_nacimiento, nacionalidad){
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.telefono= telefono;
+        this.email = email;
+        this.dni = dni;
+        this.fecha_nacimiento = fecha_nacimiento;
+        this.lugar_nacimiento = lugar_nacimiento;
+        this.nacionalidad = nacionalidad;
+
+    }
+
+    get nombre() {
+        return this.nombre;
+    }
+
+    get apellidos() {
+        return this.apellidos;
+    }
+
+    get telefono() {
+        return this.telefono;
+    }
+
+    get email() {
+        return this.email;
+    }
+
+    get dni() {
+        return this.dni;
+    }
+
+    get fecha_nacimiento() {
+        return this.fecha_nacimiento;
+    }
+
+    get lugar_nacimiento() {
+        return this.lugar_nacimiento;
+    }
+
+    get nacionalidad(){
+        return this.nacionalidad;
+    }
 }
