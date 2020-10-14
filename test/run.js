8ade010e59bc0b71e53fda26119d874e3984eed3
sergiocantero8 <sergiocantero8@gmail.com>
2020-10-14 21:21:16 +0200

    inicio de run.js #13

--- test/run.js
+++ test/run.js
@@ -1,1 +1,14 @@
-console.log("ok");
+var chai = require('chai');
+var assert = chai.assert;
+var should = chai.should();
+var expect = chai.expect;
+var reserva = require('./src/reserva');
+var usuario = require ('./usuario.js');
+
+describe('Testing assert function: ', function() {
+  describe('Check addTest Function', function(){
+    it('Comprueba que devuelve correctamente el tipo de dato : assert.equal(value, value): ', function() {
+       
+    });
+  });
+})
