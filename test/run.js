4bd55821adfa5329525097b71b3c0b4d8314a0ac
sergiocantero8 <sergiocantero8@gmail.com>
2020-10-16 00:23:49 +0200

    Añadido tests finales #13

--- test/run.js
+++ test/run.js
@@ -12,6 +12,7 @@ describe('Testeando el método ver_datosreserva: ', function() {
   let unareserva = new reserva.Reserva('Padel', 130, '14-10-2020', 15, '1234', 'Cartuja', unusuario.get_nombreyapellidos() );
   let ungestor= new gestor.GestorReservas();
 
+
   it('ver_datosreserva debe devolver una lista vacía', function(){
 
     let misreservas = ungestor.ver_datosreserva(unusuario);
@@ -36,9 +37,63 @@ describe('Testeando el método ver_datosreserva: ', function() {
   });
 
 
+
+
 });
 
 
+describe('Testeando el método add_datosreserva: ', function() {
+
+
+  let ungestor= new gestor.GestorReservas();
+  let unusuario = new usuario.Usuario('Pepito', 'Fernandez Campos', 'pepito@gmail.com', '34657239F', '04/02/1998', 'Granada', 'España' );
+  let unareserva = new reserva.Reserva('Padel', 130, '14-10-2020', 15, '1234', 'Cartuja', unusuario.get_nombreyapellidos() );
+
+  it('add_datosreserva debe tener un usuario y una reserva', function(){
+
+    ungestor.add_datosreserva(unusuario,unareserva);
+
+    assert.equal(ungestor.get_numreservas()+ungestor.get_numusuarios(), 2);
+  });
+
+
+
+});
+
+describe('Testeando gets de usuario: ', function() {
+
+
+  let unusuario = new usuario.Usuario('Pepito', 'Fernandez Campos', 'pepito@gmail.com', '34657239F', '04/02/1998', 'Granada', 'España' );
+
+  it('get_nombre debe devolver un string', function(){
+
+    assert.typeOf(unusuario.get_nombre(), "string");
+  });
+
+  it('get_nombre debe devolver el nombre correcto', function(){
+
+    assert.equal(unusuario.get_nombre(), "Pepito");
+  });
+
+
+
+});
+
+
+describe('Testeando gets de reserva: ', function() {
+
+
+  let unareserva = new reserva.Reserva('Padel', 130, '14-10-2020', 15, '1234', 'Cartuja', 'Pepito Fernandez Campos' );
+
+  it('get_tipo debe devolver un string', function(){
+
+    assert.typeOf(unareserva.get_tipo(), "string");
+  });
+
+  it('get_tipo debe devolver el tipo de pista correcto', function(){
+
+    assert.equal(unareserva.get_tipo(), "Padel");
+  });
 
 
 
