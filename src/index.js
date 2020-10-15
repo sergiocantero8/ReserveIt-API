c7121c0255258cf01128800608222895979a879d
sergiocantero8 <sergiocantero8@gmail.com>
2020-10-15 15:25:25 +0200

    Datos de prueba para clases

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 const usuario = require ('./usuario.js')
-const Reserva = require ('./reserva.js')
+const reserva = require ('./reserva.js')
+const gestor = require ('./gestor_reservas.js')
 
 
 let unusuario = new usuario.Usuario('Sergio', 'Azanon Cantero', 'sergiocantero8@gmail.com', '76589409F', '04/02/1998', 'Granada', 'España' );
+let unareserva = new reserva.Reserva('Padel', 130, '14-10-2020', 15, '1234', 'Cartuja', unusuario.get_nombreyapellidos() );
+let ungestor= new gestor.GestorReservas();
+
+ungestor.add_datosreserva(unusuario,unareserva);
+
+ungestor.ver_todosdatosreserva();
+
+ungestor.ver_datosreserva(unusuario);
+
 console.log(unusuario.get_nombre());
+console.log(unareserva.get_usuario());
