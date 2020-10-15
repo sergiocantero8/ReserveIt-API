ea1117cb753bdb91a864f5f448013bcaeac2dadc
sergiocantero8 <sergiocantero8@gmail.com>
2020-10-16 00:02:42 +0200

    Testeado primer metodo para ver los datos de una reserva #13

--- test/run.js
+++ test/run.js
@@ -2,13 +2,44 @@ var chai = require('chai');
 var assert = chai.assert;
 var should = chai.should();
 var expect = chai.expect;
-var reserva = require('./src/reserva');
-var usuario = require ('./usuario.js');
+var reserva = require('../src/reserva');
+var usuario = require ('../src/usuario.js');
+var gestor = require ('../src/gestor_reservas.js')
 
-describe('Testing assert function: ', function() {
-  describe('Check addTest Function', function(){
-    it('Comprueba que devuelve correctamente el tipo de dato : assert.equal(value, value): ', function() {
-       
-    });
+describe('Testeando el método ver_datosreserva: ', function() {
+
+  let unusuario = new usuario.Usuario('Pepito', 'Fernandez Campos', 'pepito@gmail.com', '34657239F', '04/02/1998', 'Granada', 'España' );
+  let unareserva = new reserva.Reserva('Padel', 130, '14-10-2020', 15, '1234', 'Cartuja', unusuario.get_nombreyapellidos() );
+  let ungestor= new gestor.GestorReservas();
+
+  it('ver_datosreserva debe devolver una lista vacía', function(){
+
+    let misreservas = ungestor.ver_datosreserva(unusuario);
+    
+    assert.equal(misreservas.length, 0);
   });
-})
+
+  it('ver_datosreserva debe devolver una lista con un elemento', function(){
+
+    ungestor.add_datosreserva(unusuario,unareserva);
+    let misreservas = ungestor.ver_datosreserva(unusuario);
+    
+    assert.equal(misreservas.length, 1);
+  });
+
+  it('ver_datosreserva debe devolver correctamente los datos', function(){
+
+    ungestor.add_datosreserva(unusuario,unareserva);
+    let misreservas = ungestor.ver_datosreserva(unusuario);
+    
+    assert.equal(misreservas[0].get_usuario(), 'Pepito Fernandez Campos');
+  });
+
+
+});
+
+
+
+
+
+});
