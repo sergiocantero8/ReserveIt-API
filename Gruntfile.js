cb850aff86d43b730ecb862530466567cca1ff29
sergiocantero8 <sergiocantero8@gmail.com>
2020-10-19 16:28:44 +0200

    configurado el gruntfile.js para trabajar con el task-runner grunt closes #18

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// Contendrá todo nuestro código 
+module.exports = function(grunt) {
+
+    // Inicializa un objeto de configuración para el proyecto actual
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        mochaTest: {
+
+            test: {
+                options: {
+                  reporter: 'spec',
+                },
+                src: ['test/**/*.js']
+            }
+        }
+    });
+
+    // Carga los plugins de Grunt
+    grunt.loadNpmTasks('grunt-mocha-test');
+
+    // Registra las tareas
+    grunt.registerTask('default', ['mochaTest']);
+    
+};
