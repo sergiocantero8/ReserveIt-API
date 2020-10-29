2a82260ce530d92aa1d76176a7d2fe6d43dc43cd
sergiocantero8 <sergiocantero8@gmail.com>
2020-10-29 19:25:31 +0100

    añadido setters de reserva closes #26

--- src/reserva.js
+++ src/reserva.js
@@ -46,6 +46,34 @@ class Reserva {
         return "Tipo de pista: " + this.tipo + " Duracion: " + this.duracion + " mins"+ " Fecha: " + this.fecha + " Precio: " + this.precio + " euros" + 
                 " ID: " + this.id + " Ubicacion: " + this.ubicacion + " Usuario: " + this.usuario;
     }
+
+    set_tipo(tipo){
+        this.tipo=tipo;
+    }
+
+    set_duracion(duracion){
+        this.duracion=duracion;
+    }
+
+    set_fecha(fecha){
+        this.fecha=fecha;
+    }
+
+    set_precio(precio){
+        this.precio=precio;
+    }
+
+    set_id(id){
+        this.id=id;
+    }
+
+    set_ubicacion(ubicacion){
+        this.ubicacion=ubicacion;
+    }
+
+    set_usuario(usuario){
+        this.usuario=usuario;
+    }
 }
 
 module.exports.Reserva = Reserva;
