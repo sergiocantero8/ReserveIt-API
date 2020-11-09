7f0434378aea34de32430abe60093e1d5878d2c9
sergiocantero8 <sergiocantero8@gmail.com>
2020-11-09 18:58:33 +0100

    test del metodo para cancelar una reserva #37 #36

--- test/run.js
+++ test/run.js
@@ -125,3 +125,31 @@ describe('Testeando el método ver_pistalibre: ', function() {
 
 
 });
+
+
+describe('Testeando el método cancelar_pista: ', function() {
+
+
+  let ungestor= new gestor.GestorReservas();
+  let unusuario = new usuario.Usuario('Pepito', 'Fernandez Campos', 'pepito@gmail.com', '34657239F', '04/02/1998', 'Granada', 'España' );
+  let unareserva = new reserva.Reserva('Padel', 130, '14-10-2020', 15, '1234', 'Cartuja', unusuario.get_nombreyapellidos() );
+  ungestor.add_datosreserva(unusuario,unareserva);
+
+  it('cancelar_pista debe devolver true porque la reserva se ha cancelado correctamente', function(){
+
+    resultado=ungestor.cancelar_reserva(unusuario, unareserva.get_ubicacion(), unareserva.get_fecha());
+
+    assert.equal(resultado, true);
+  });
+
+  it('cancelar_pista debe devolver false porque no hay ninguna reserva que cancelar', function(){
+
+    ungestor.cancelar_reserva(unusuario, unareserva.get_ubicacion(), unareserva.get_fecha());
+    resultado=ungestor.cancelar_reserva(unusuario, unareserva.get_ubicacion(), unareserva.get_fecha());
+
+
+    assert.equal(resultado, false);
+  });
+
+
+});
