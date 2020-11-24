db90c7b2c62b2a9e193fed8ca331422339e52b33
sergiocantero8 <sergiocantero8@gmail.com>
2020-11-24 20:52:17 +0100

    reorganizacion carpeta para despliegue en vercel #49

--- telegramBot/bot.js
+++
@@ -1,9 +0,0 @@
-const Telegraf = require('telegraf')
-
-const bot = new Telegraf('1392559329:AAGjThwPqVxbbLXk5zQBcsVRhEDRUuXpjdY')
-
-bot.start((ctx) => {
-    ctx.reply('Welcome');
-})
-
-bot.launch()
