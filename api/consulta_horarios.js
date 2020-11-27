763035f48ab01163a07648358ecfcbe5769d3de1
sergiocantero8 <sergiocantero8@gmail.com>
2020-11-27 12:06:07 +0100

    eliminados archivos del gitignore

---
+++ api/consulta_horarios.js
@@ -0,0 +1,20 @@
+const data = require('./horarios.json')
+module.exports = (req, res) => {
+  var obj = {};
+  var ubicacion = req.query;
+  if(ubicacion == undefined){
+    res.status(400).send(`Debe indicar una ubicacion para informar sobre el horario`);
+  }
+  else{
+    var claves=Object.keys(data);
+    if(claves.includes(ubicacion)){
+      var horarios_ubicacion = data[ubicacion];
+      obj[ubicacion] = horarios_ubicacion;
+    }
+    else{
+      res.status(400).send(`No hay ningun horario para esa ubicacion`);
+    }
+  }
+  res.setHeader('Content-Type','application/json'); 
+  res.status(200).json(obj);
+};
