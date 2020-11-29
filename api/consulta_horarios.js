dbf63dd650bbbcf8289fc36c600eddd242b4f814
sergiocantero8 <sergiocantero8@gmail.com>
2020-11-29 13:37:03 +0100

    probando funcion #50

--- api/consulta_horarios.js
+++ api/consulta_horarios.js
@@ -6,14 +6,8 @@ module.exports = (req, res) => {
     res.status(400).send(`Debe indicar una ubicacion para informar sobre el horario`);
   }
   else{
-    var claves=Object.keys(data);
-    if(claves.includes(ubicacion)){
-      var horarios_ubicacion = data[ubicacion];
-      obj[ubicacion].push(horarios_ubicacion);
-    }
-    else{
+
       res.status(400).send(`No hay ningun horario para esa ubicacion`);
-    }
   }
 
   
