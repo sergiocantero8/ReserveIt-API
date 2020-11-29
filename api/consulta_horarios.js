1f156103d92a0e283c15c99327cb421bc9057ee5
sergiocantero8 <sergiocantero8@gmail.com>
2020-11-29 13:44:20 +0100

    arreglado error en headers #50

--- api/consulta_horarios.js
+++ api/consulta_horarios.js
@@ -2,14 +2,29 @@ const data = require("./horarios.json")
 module.exports = (req, res) => {
   var obj = {};
   var ubicacion = req.query;
+  let status;
+  let mensaje;
   if(ubicacion == undefined){
-    res.status(400).send(`Debe indicar una ubicacion para informar sobre el horario`);
+    status=400;
+    mensaje="Debe indicar una ubicacion para mostrar el horario"
   }
   else{
-
-      res.status(400).send(`No hay ningun horario para esa ubicacion`);
+    var claves=Object.keys(data);
+    if(claves.includes(ubicacion)){
+      var horarios_ubicacion = data[ubicacion];
+      obj[ubicacion].push(horarios_ubicacion);
+      status=200;
+    }
+    else{
+      status=400;
+      mensaje="No hay horarios para esa pista"
+    }
   }
+  res.setHeader('Content-Type','application/json'); 
 
-  
-  res.status(200).send(obj);
+  if(mensaje.length== 0)
+    res.status(status).send(obj);
+  else
+    res.status(status).send(mensaje);
+
 }
