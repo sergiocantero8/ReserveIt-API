e91b410edf330bfe2b31daf6ae5f857e6eeb1b40
sergiocantero8 <sergiocantero8@gmail.com>
2020-12-11 12:22:54 +0100

    ruta para reservar un polideportivo #8

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ const express= require('express');
 const app = express();
 const morgan = require('morgan');
 
-const {Gestor} = require("./gestor_reservas.js");
+const { GestorReservas } = require("./gestor_reservas.js");
+const {Reserva} = require("./reserva.js");
+const {Usuario} = require("./usuario.js");
 
-var controlador = new Gestor();
+var gestor = new GestorReservas();
 
 
 // Middlewares
@@ -23,6 +25,16 @@ app.use(express.json());
 app.get('/', (req,res) => {
     res.status(200).send("¡Bienvenido a la API de reserve-it!");
 });
+
+// Crea una reserva
+app.put('/reservar/:tipo/:duracion/:fecha/:ubicacion/:dni_usuario', function( req, response ) {
+    var nueva_reserva = new Reserva(req.params.tipo,req.params.duracion,
+                      req.params.fecha, req.params.ubicacion, req.params.dni_usuario );
+    var nuevo_usuario= new Usuario('Pepito', 'Fernandez Campos', 'pepito@gmail.com', '34657239F', '04/02/1998', 'Granada', 'España' );
+    gestor.add_datosreserva( nuevo_usuario, nueva_reserva );
+    response.status(200).send( nueva_porra );
+});
+
 
 
 app.listen(app.get('port'), () => {
