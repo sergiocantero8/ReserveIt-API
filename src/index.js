8c7d3363d941e48b599b5e0fc299472937d07b4e
sergiocantero8 <sergiocantero8@gmail.com>
2020-12-12 14:02:12 +0100

    usuario sin registrar añadido para poder reservar #8

--- src/index.js
+++ src/index.js
@@ -12,11 +12,10 @@ var gestor = new GestorReservas();
 // Middlewares
 app.use(morgan('combined'));
 
-// Establezco el puerto
+// Establezco el IP y el puerto
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'; 
 app.set('port', process.env.PORT || 4000);
 
-// Para que no soporte cosas pesadas como imagenes
-app.use(express.urlencoded({extended:false}));
 
 // Para enviar contenido en formato JSON
 app.use(express.json());
@@ -30,30 +29,29 @@ app.get('/', (req,res) => {
 
 // Crea una reserva
 app.put('/reservar/:tipo/:duracion/:fecha/:precio/:ubicacion/:dni_usuario', function( req, response ) {
+
     var id = gestor.get_numreservas()+1;
     var nueva_reserva = new Reserva(req.params.tipo,req.params.duracion,
                       req.params.fecha,req.params.precio, id, req.params.ubicacion, req.params.dni_usuario );
-    var nuevo_usuario= new Usuario('Pepito', 'Fernandez Campos', 'pepito@gmail.com', '34657239F', '04/02/1998', 'Granada', 'España' );
-    gestor.add_datosreserva( nuevo_usuario, nueva_reserva );
-    response.status(200).json( nueva_reserva);
-});
+    if(gestor.usuario_con_reserva(req.params.dni_usuario)){
+        var usuario_registrado=get_Usuario(req.params.dni_usuario);
+        gestor.add_datosreserva( usuario_registrado, nueva_reserva );
+    }
+    else{
+        var usuario_sin_registrar= new Usuario('undefined', 'undefined', 'undefined', req.params.dni_usuario, 'undefined', 'undefined', 'undefined' );
+        gestor.add_datosreserva( usuario_sin_registrar, nueva_reserva );
+    }
+    
+    
+    response.status(201).json( nueva_reserva);
 
-// Consultar todas las reservas
-app.get('/consultar_reservas', (req,res) => {
-
-    var mis_reservas = gestor.ver_todosdatosreserva();
-
-    /*
-    _.each(mis_reservas, (reserva, i) =>{
-        
-    });
-    */
-    res.status(200).send(mis_reservas);
 });
 
 
 
 
-app.listen(app.get('port'), () => {
-    console.log(`Server on port ${app.get('port')}`);
+
+
+app.listen(app.get('port'),server_ip_address,function () {
+    console.log(`La aplicacion se está ejecutando en  ` + server_ip_address + ":" +   app.get('port'));
 });
